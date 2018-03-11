c70bbcf5043eb142ca1f489b6afd52745ae44e6c
敲一手烂代码 <mayu0418@163.com>
2018-03-11 17:44:41 +0800

    2至2.3.3

---
+++ 2_233.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 10 16:06:22 2018
+
+@author: mayu
+"""
+
+import numpy as np
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+sys.path.append(r'E:\Python\introduction_to_ml_with_python-master\introduction_to_ml_with_python-master')
+import mglearn 
+
+X,y = mglearn.datasets.make_forge()
+mglearn.discrete_scatter(X[:,0],X[:,1],y)
+plt.legend(["class 1","class 2"],loc=4)
+plt.xlabel("first feature")
+plt.ylabel("second feature")
+print(X.shape)
+
+X,y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X,y,'o')
+plt.ylim(-3,3)
+plt.xlabel("feature")
+plt.ylabel("target")
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+print(cancer.keys())
+print(cancer.data.shape)
+print({n:v for n,v in zip(cancer.target_names,np.bincount(cancer.target))})
+print(cancer.feature_names)
+
+from sklearn.datasets import load_boston
+boston = load_boston()
+print(boston.data.shape)
+
+X,y = mglearn.datasets.load_extended_boston()
+print(X.shape)
+
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+from sklearn.model_selection import train_test_split
+X,y = mglearn.datasets.make_forge()
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train,y_train)
+print(clf.predict(X_test))
+print(clf.score(X_test,y_test))
+
+fig,axes = plt.subplots(1,3)
+for n_neighbors,ax in zip([1,3,9],axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X,y)
+    mglearn.plots.plot_2d_separator(clf,X,fill=True,eps=0.5,ax=ax,alpha=.4)
+    mglearn.discrete_scatter(X[:,0],X[:,1],y,ax=ax)
+    ax.set_title("{}".format(n_neighbors))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axes[0].legend(loc=3)
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,
+                                                 stratify=cancer.target,random_state=66)
+training_accuracy = []
+test_accuracy = []
+neighbors_settings = range(1,11)
+for n_neighbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train,y_train)
+    training_accuracy.append(clf.score(X_train,y_train))
+    test_accuracy.append(clf.score(X_test,y_test))
+plt.plot(neighbors_settings,training_accuracy,label="training accuracy")
+plt.plot(neighbors_settings,test_accuracy,label="test accuracy")
+plt.ylabel("accuracy")
+plt.xlabel("neighbors")
+plt.legend()
+
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+mglearn.plots.plot_knn_regression(n_neighbors=3)
+
+from sklearn.neighbors import KNeighborsRegressor
+X,y = mglearn.datasets.make_wave(n_samples=40)
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+reg = KNeighborsRegressor(n_neighbors=3)
+reg.fit(X_train,y_train)
+print(reg.predict(X_test))
+print(reg.score(X_test,y_test))
+
+fig,axes = plt.subplots(1,3,figsize=(15,4))
+line = np.linspace(-3,3,1000).reshape(-1,1)
+for n_neighbors,ax in zip([1,3,9],axes):
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train,y_train)
+    ax.plot(line,reg.predict(line))
+    ax.plot(X_train,y_train,'^',c=mglearn.cm2(0),markersize=8)
+    ax.plot(X_test,y_test,'v',c=mglearn.cm2(1),markersize=8)
+    ax.set_title("{} neighbor(s)\n train score:{:.2f} test score:{:.2f}".format(n_neighbors,
+                 reg.score(X_train,y_train),
+                 reg.score(X_test,y_test)))
+    ax.set_xlabel("feature")
+    ax.set_ylabel("target")
+axes[0].legend(["Model","Training","Test"],loc="best")
+
+mglearn.plots.plot_linear_regression_wave()
+
+from sklearn.linear_model import LinearRegression
+X,y = mglearn.datasets.make_wave(n_samples=60)
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=42)
+lr = LinearRegression().fit(X_train,y_train)
+print(lr.coef_)
+print(lr.intercept_)
+print(lr.score(X_train,y_train))
+print(lr.score(X_test,y_test))
+
+X,y = mglearn.datasets.load_extended_boston()
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+lr = LinearRegression().fit(X_train,y_train)
+print(lr.score(X_train,y_train))
+print(lr.score(X_test,y_test))
+
+from sklearn.linear_model import Ridge
+ridge = Ridge().fit(X_train,y_train)
+print(ridge.score(X_train,y_train))
+print(ridge.score(X_test,y_test))
+
+ridge10 = Ridge(alpha=10).fit(X_train,y_train)
+print(ridge10.score(X_train,y_train))
+print(ridge10.score(X_test,y_test))
+
+ridge01 = Ridge(alpha=0.1).fit(X_train,y_train)
+print(ridge01.score(X_train,y_train))
+print(ridge01.score(X_test,y_test))
+
+plt.plot(ridge.coef_,'s',label="alpha=1")
+plt.plot(ridge01.coef_,'^',label="alpha=0.1")
+plt.plot(ridge10.coef_,'v',label="alpha=10")
+
+plt.plot(lr.coef_,'o',label="lr")
+plt.xlabel("index")
+plt.ylabel("result")
+plt.hlines(0,0,len(lr.coef_))
+plt.ylim(-25,25)
+plt.legend()
+
+mglearn.plots.plot_ridge_n_samples()
+
+from sklearn.linear_model import Lasso
+
+lasso = Lasso().fit(X_train,y_train)
+print(lasso.score(X_train,y_train))
+print(lasso.score(X_test,y_test))
+print(np.sum(lasso.coef_ != 0))
+
+lasso001 = Lasso(alpha=0.01,max_iter=100000).fit(X_train,y_train)
+print(lasso001.score(X_train,y_train))
+print(lasso001.score(X_test,y_test))
+print(np.sum(lasso001.coef_ != 0))
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+X,y = mglearn.datasets.make_forge()
+fig,axes = plt.subplots(1,2,figsize=(10,3))
+for model,ax in zip([LinearSVC(),LogisticRegression()],axes):
+    clf = model.fit(X,y)
+    mglearn.plots.plot_2d_separator(clf,X,fill=False,eps=0.5,ax=ax,alpha=.7)
+    mglearn.discrete_scatter(X[:,0],X[:,1],y,ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axes[0].legend()
+
+from sklearn.datasets import make_blobs
+X,y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:,0],X[:,1],y)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+plt.legend(["class 0","class 1","class 2",])
+
+linear_svm = LinearSVC().fit(X,y)
+print(linear_svm.coef_.shape)
+print(linear_svm.intercept_.shape)
+
+mglearn.discrete_scatter(X[:,0],X[:,1],y)
+line = np.linspace(-15,15)
+for coef,intercept,color in zip(linear_svm.coef_,
+                                linear_svm.intercept_,
+                                ['b','r','g']):
+    plt.plot(line,-(line * coef[0] + intercept) / coef[1],c=color)
+plt.ylim(-10,15)
+plt.xlim(-10.8)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+plt.legend(["class 0","class 1","class 2","line class 0","line class 1","line class 2",])
+
+mglearn.plots.plot_2d_classification(linear_svm,X,fill=True,alpha=.7)
+mglearn.discrete_scatter(X[:,0],X[:,1],y)
+line = np.linspace(-15,15)
+for coef,intercept,color in zip(linear_svm.coef_,
+                                linear_svm.intercept_,
+                                ['b','r','g']):
+    plt.plot(line,-(line * coef[0] + intercept) / coef[1],c=color)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+plt.legend(["class 0","class 1","class 2","line class 0","line class 1","line class 2",])
