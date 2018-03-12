ca9119ecc2d0c48af8aedcdf91b66c4024472e3a
敲一手烂代码 <mayu0418@163.com>
2018-03-12 23:15:23 +0800

    读至2

--- 2.py
+++ 2.py
@@ -209,3 +209,67 @@ for coef,intercept,color in zip(linear_svm.coef_,
 plt.xlabel("feature 0")
 plt.ylabel("feature 1")
 plt.legend(["class 0","class 1","class 2","line class 0","line class 1","line class 2",])
+
+mglearn.plots.plot_animal_tree()
+
+from sklearn.tree import DecisionTreeClassifier
+
+cancer = load_breast_cancer()
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,
+                                                 stratify=cancer.target,random_state=0)
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(X_train,y_train)
+print(tree.score(X_train,y_train))
+print(tree.score(X_test,y_test))
+
+from sklearn.tree import export_graphviz
+export_graphviz(tree,out_file="tree.dot",class_names=["malignant","benign"])
+
+import graphviz
+
+with open("tree.dot") as f:
+    dot_graph = f.read()
+graphviz.Source(dot_graph)
+
+print(tree.feature_importances_)
+
+def plot_feature_importances_cancer(model):
+    n_features = cancer.data.shape[1]
+    plt.barh(range(n_features),model.feature_importances_,align='center')
+    plt.yticks(np.arange(n_features),cancer.feature_names)
+    plt.xlabel("feature importance")
+    plt.ylabel("feature")
+plot_feature_importances_cancer(tree)
+
+tree = mglearn.plots.plot_tree_not_monotone()
+display(tree)
+
+import pandas as pd
+ram_prices = pd.read_csv(r'E:/Python/introduction_to_ml_with_python-master/introduction_to_ml_with_python-master/data/ram_price.csv')
+plt.semilogy(ram_prices.date,ram_prices.price)
+plt.xlabel("Year")
+plt.ylabel("Price")
+
+from sklearn.tree import DecisionTreeRegressor
+data_train = ram_prices[ram_prices.date < 2000]
+data_test = ram_prices[ram_prices.date >= 2000]
+
+X_train = data_train.date[:,np.newaxis]
+y_train = np.log(data_train.price)
+
+tree= DecisionTreeRegressor().fit(X_train,y_train)
+linear_reg = LinearRegression().fit(X_train,y_train)
+
+X_all = ram_prices.date[:,np.newaxis]
+
+pred_tree = tree.predict(X_all)
+pred_lr = linear_reg.predict(X_all)
+
+price_tree = np.exp(pred_tree)
+price_lr = np.exp(pred_lr)
+
+plt.semilogy(data_train.date,data_train.price,label="training date")
+plt.semilogy(data_test.date,data_test.price,label="test date")
+plt.semilogy(ram_prices.date,price_tree,label="Tree")
+plt.semilogy(ram_prices.date,price_lr,label="Lr")
+plt.legend()
