f74f1b2af0fb8985578e36a3b7bcc40548f24b8e
敲一手烂代码 <mayu0418@163.com>
2018-03-14 22:54:03 +0800

    读至2.3.6

--- 2.py
+++ 2.py
@@ -273,3 +273,48 @@ plt.semilogy(data_test.date,data_test.price,label="test date")
 plt.semilogy(ram_prices.date,price_tree,label="Tree")
 plt.semilogy(ram_prices.date,price_lr,label="Lr")
 plt.legend()
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_moons
+X,y = make_moons(n_samples=100,noise=0.25,random_state=3)
+X_train,X_test,y_train,y_test = train_test_split(X,y,stratify=y,random_state=42)
+forest = RandomForestClassifier(n_estimators=5,random_state=2)
+forest.fit(X_train,y_train)
+print(len(forest.estimators_))
+
+fig,axes = plt.subplots(2,3,figsize=(20,10))
+for i,(ax,tree) in enumerate(zip(axes.ravel(),forest.estimators_)):
+    ax.set_title("Tree {}".format(i))
+    mglearn.plots.plot_tree_partition(X_train,y_train,tree,ax=ax)
+mglearn.plots.plot_2d_separator(forest,X_train,fill=True,ax=axes[-1,1],alpha=.4)
+axes[-1,-1].set_title("Random Forest")
+mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train)
+
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,
+                                                 cancer.target,
+                                                 random_state=0)
+forest.fit(X_train,y_train)
+print(forest.score(X_train,y_train))
+print(forest.score(X_test,y_test))
+
+plot_feature_importances_cancer(forest)
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,random_state=0)
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train,y_train)
+print(gbrt.score(X_train,y_train))
+print(gbrt.score(X_test,y_test))
+
+gbrt = GradientBoostingClassifier(random_state=0,max_depth=1)
+gbrt.fit(X_train,y_train)
+print(gbrt.score(X_train,y_train))
+print(gbrt.score(X_test,y_test))
+
+gbrt = GradientBoostingClassifier(random_state=0,learning_rate=0.01)
+gbrt.fit(X_train,y_train)
+print(gbrt.score(X_train,y_train))
+print(gbrt.score(X_test,y_test))
+
+plot_feature_importances_cancer(gbrt)
