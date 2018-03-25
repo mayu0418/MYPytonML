4924d3753552d3324d81fbdd6f4d84037c4db26b
敲一手烂代码 <mayu0418@163.com>
2018-03-25 19:02:07 +0800

    读至2.4.1

--- 2.py
+++ 2.py
@@ -429,3 +429,112 @@ svc.fit(X_train_scaled,y_train)
 
 print(svc.score(X_train_scaled,y_train))
 print(svc.score(X_test_scaled,y_test))
+
+display(mglearn.plots.plot_logistic_regression_graph())
+
+display(mglearn.plots.plot_single_hidden_layer_graph())
+
+line = np.linspace(-3,3,100)
+plt.plot(line,np.tanh(line),label="tanh")
+plt.plot(line,np.maximum(line,0),label="relu")
+plt.legend(loc="best")
+plt.xlabel("x")
+plt.ylabel("relu(x),tanh(x)")
+
+mglearn.plots.plot_two_hidden_layer_graph()
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_moons
+X,y = make_moons(n_samples=100,noise=0.25,random_state=3)
+X_train,X_test,y_train,y_test = train_test_split(X,y,stratify=y,random_state=42)
+mlp = MLPClassifier(solver='lbfgs',random_state=0).fit(X_train,y_train)
+mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3)
+mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+
+mlp = MLPClassifier(solver='lbfgs',random_state=0,hidden_layer_sizes=[10]).fit(X_train,y_train)
+mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3)
+mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+
+mlp = MLPClassifier(solver='lbfgs',random_state=0,hidden_layer_sizes=[10,10]).fit(X_train,y_train)
+mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3)
+mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+
+mlp = MLPClassifier(solver='lbfgs',random_state=0,hidden_layer_sizes=[10,10]).fit(X_train,y_train)
+mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3)
+mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train)
+plt.xlabel("feature 0")
+plt.ylabel("feature 1")
+
+fig,axes = plt.subplots(2,4,figsize=(20,8))
+for axx,n_hidden_nodes in zip(axes,[10,100]):
+    for ax,alpha in zip(axx,[0.0001,0.01,0.1,1]):
+        mlp = MLPClassifier(solver='lbfgs',random_state=0,
+                            hidden_layer_sizes=[n_hidden_nodes,n_hidden_nodes],
+                            alpha=alpha)
+        mlp.fit(X_train,y_train)
+        mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3,ax=ax)
+        mglearn.discrete_scatter(X_train[:,0],X _train[:,1],y_train,ax=ax)
+        ax.set_title("n_hidden=[{},{}]\nalpha={:.4f}".format(n_hidden_nodes,n_hidden_nodes,alpha))
+        
+fig,axes = plt.subplots(2,4,figsize=(20,8))
+for i,ax in enumerate(axes.ravel()):
+    mlp = MLPClassifier(solver='lbfgs',random_state=i,hidden_layer_sizes=[100,100])
+    mlp.fit(X_train,y_train)
+    mglearn.plots.plot_2d_separator(mlp,X_train,fill=True,alpha=.3,ax=ax)
+    mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train,ax=ax)
+    
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,random_state=0)
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train,y_train)
+print(mlp.score(X_train,y_train))
+print(mlp.score(X_test,y_test))
+
+plt.figure(figsize=(20,5))
+plt.imshow(mlp.coefs_[0],interpola tion='none',cmap='viridis')
+plt.yticks(range(30),cancer.feature_names)
+plt.xlabel("columns in weight matrix")
+plt.ylabel("input feature")
+plt.colorbar
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import make_circles
+
+X,y = make_circles(noise=0.25,factor=0.5,random_state=1)
+y_named = np.array(["blue","red"])[y]
+
+X_train,X_test,y_train_named,y_test_named,y_train,y_test = train_test_split(X,y_named,y,random_state=0)
+
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train,y_train_named)
+
+print(X_test.shape)
+print(gbrt.decision_function(X_test).shape)
+
+print(gbrt.decision_function(X_test)[:6])
+
+print(gbrt.decision_function(X_test) > 0)
+print(gbrt.predict(X_test))
+
+greater_zero = (gbrt.decision_function(X_test) > 0).astype(int)
+pred = gbrt.classes_[greater_zero]
+print(np.all(pred == gbrt.predict(X_test)))
+
+decision_function = gbrt.decision_function(X_test)
+print("minimum:{:.2f} maximum:{:.2f}".format(np.min(decision_function),np.max(decision_function)))
+
+fig,axes = plt.subplots(1,2,figsize=(13,5))
+mglearn.tools.plot_2d_separator(gbrt,X,ax=axes[0],alpha=.4,fill=True,cm=mglearn.cm2)
+scores_image = mglearn.tools.plot_2d_scores(gbrt,X,ax=axes[1],alpha=.4,cm=mglearn.ReBl)
+for ax in axes:
+    mglearn.discrete_scatter(X_test[:,0],X_test[:,1],y_test,markers='^',ax=ax)
+    mglearn.discrete_scatter(X_train[:,0],X_train[:,1],y_train,markers='o',ax=ax)
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+cbar = plt.colorbar(scores_image,ax=axes.tolist())
+axes[0].legend(["test class 0","test class 1","train class 0","train class 1"],ncol=4,loc=(.1,1.1))
